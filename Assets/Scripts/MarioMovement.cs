c702c91414f4fcceeec64d02719cceab0275a486
Tom Keir <tomkeir920@icloud.com>
2023-06-17 11:04:30 +1000

    Cleaned up code
    Player Movement code has now been broken down into separate functions. It can now be read and understand easier and new comments help clarify certain functions.

--- Assets/Scripts/MarioMovement.cs
+++ Assets/Scripts/MarioMovement.cs
@@ -10,105 +10,115 @@ public class MarioMovement : MonoBehaviour
     public Transform playerObject;
     public Rigidbody rb;
 
-    public float rotationSpeed;
-
-    
-    
     [Header("Physics")]
     public float groundDrag;
 
     [Header("Input")]
-    float horizontalInput;
-    float verticalInput;
-    public float moveMagnitude;
+    private float horizontalInput;
+    private float verticalInput;
+    private float moveMagnitude;
 
     [Header("Movement")]
     public float moveSpeed;
     public float jumpForce;
     public float jumpCooldown;
     public float airMultiplier;
-    public bool readyToJump;
-    public bool readyToLongJump;
-    public bool longJumping;
     public float longJumpForceMultiplier;
     public float longJumpSpeedMultiplier;
     public int ticksAfterJump;
+    public float rotationSpeed;
 
     [Header("Score")]
     public int coins;
     public int redCoins;
 
-    Vector3 moveDirection;
-
     [Header("Ground Check")]
     public float playerHeight;
     public LayerMask ground;
-    private bool grounded;
+    public bool grounded;
+    public bool readyToJump;
+    public bool readyToLongJump;
+    private bool longJumping;
 
     private void Start()
     {
         rb.freezeRotation = true;
     }
-    
+
     private void Update()
-    {   
+    {
+        HandleInput();
+        GroundCheck();
+        RotatePlayerOrientation();
+        MovePlayer();
         SpeedControl();
-        //ground check
+    }
+
+
+    private void HandleInput()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        moveMagnitude = Mathf.Clamp01(new Vector2(horizontalInput, verticalInput).magnitude);
+    }
+
+    private void GroundCheck()
+    {
         grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, ground);
-        
-        //rotate player's orientation
+    }
+
+    private void RotatePlayerOrientation()
+    {
         Vector3 viewDir = player.position - transform.position;
         if (viewDir.sqrMagnitude > 0.001f)
         {
             Quaternion targetRotation = Quaternion.LookRotation(viewDir, Vector3.up);
             orientation.rotation = Quaternion.Slerp(orientation.rotation, targetRotation, rotationSpeed * Time.deltaTime);
         }
+    }
 
-        //rotate the player object
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+    private void MovePlayer()
+    {
+        
         Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
-
-        if (ticksAfterJump < 99) {
-            ticksAfterJump += 1;
-        }
-        
-
-        //when to long jump
-        if (Input.GetButton("Jump") && Input.GetButton("Fire3") && readyToLongJump && grounded)
-        {   
-            Debug.Log("Long Jump");
-            longJumping = true;
-            readyToJump = false;
-            readyToLongJump = false;
-            Jump(longJumpForceMultiplier);
-            Invoke(nameof(ResetJump), jumpCooldown);
-            rb.AddForce(moveDirection * moveSpeed * longJumpSpeedMultiplier, ForceMode.Acceleration);
-        }
-
-        //when to jump
-        else if (Input.GetButton("Jump") && readyToJump && grounded )
+        if (ticksAfterJump < 99)
         {
-            Debug.Log("Jump");
-            readyToJump = false;
-            readyToLongJump = false;
-            Jump(1f);
-            Invoke(nameof(ResetJump), jumpCooldown);
-            ticksAfterJump = 0;
-            
+            ticksAfterJump++;
         }
-        
 
-        //calculate movement direction
-        moveDirection = inputDir.normalized;
-        moveMagnitude = Mathf.Clamp01(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).magnitude); //checks for how far the analog stick is being pushed. If playing on a keyboard, magnitude is either a one or a zero anyway, so this does not affect that.
+        //check jump inputs
+        if (Input.GetButton("Jump") && Input.GetButton("Fire3") && readyToLongJump && grounded)
+        {
+            PerformLongJump(inputDir);
+        }
+        else if (Input.GetButton("Jump") && readyToJump && grounded)
+        {
+            PerformJump();
+        }
+
+        //jump and long jump
+        MoveDirection(inputDir);
+
+        if (Input.GetButtonDown("Jump") && readyToJump && grounded)
+        {
+            PerformJump();
+        }
+        else if (Input.GetButtonDown("Jump") && Input.GetButton("Fire3") && readyToLongJump && grounded)
+        {
+            PerformLongJump(inputDir);
+        }
+    }
+
+    private void MoveDirection(Vector3 inputDir)
+    {
+        //move player in direction
+        Vector3 moveDirection = inputDir.normalized;
 
         if (grounded)
         {
             rb.AddForce(moveDirection * moveMagnitude * moveSpeed, ForceMode.Acceleration);
-
         }
-        else if (!grounded)
+        else
         {
             rb.AddForce(moveDirection * moveMagnitude * moveSpeed * airMultiplier, ForceMode.Acceleration);
         }
@@ -118,57 +128,60 @@ public class MarioMovement : MonoBehaviour
             playerObject.forward = Vector3.Slerp(playerObject.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
         }
 
-        //ground check
-        if (grounded)
-        {
-            
-            rb.drag = groundDrag;
-        }
-        else
-        {
-            rb.drag = 0;
-        }
+        rb.drag = grounded ? groundDrag : 0;
+    }
+
+    private void PerformLongJump(Vector3 inputDir)
+    {
+        Debug.Log("Long Jump");
+        longJumping = true;
+        readyToJump = false;
+        readyToLongJump = false;
+        Jump(longJumpForceMultiplier);
+        Invoke(nameof(ResetJump), jumpCooldown);
+        rb.AddForce(inputDir * moveSpeed * longJumpSpeedMultiplier, ForceMode.Acceleration);
+    }
+
+    private void PerformJump()
+    {
+        Debug.Log("Jump");
+        readyToJump = false;
+        readyToLongJump = false;
+        Jump(1f);
+        Invoke(nameof(ResetJump), jumpCooldown);
+        ticksAfterJump = 0;
+    }
+
+    private void Jump(float jumpMultiplier)
+    {
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        rb.AddForce(transform.up * jumpForce * jumpMultiplier, ForceMode.Impulse);
+    }
+
+    private void ResetJump()
+    {
+        readyToJump = true;
+        readyToLongJump = true;
+        longJumping = false;
     }
 
     private void SpeedControl()
     {
+        //cap player's speed unless long jumping
         float maxSpeed = moveSpeed;
 
         if (!grounded && longJumping)
         {
             maxSpeed *= longJumpSpeedMultiplier;
-            rb.AddForce(moveDirection * moveMagnitude * moveSpeed, ForceMode.Acceleration);
+            rb.AddForce(rb.velocity.normalized * moveMagnitude * moveSpeed, ForceMode.Acceleration);
         }
 
         Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
 
-        //limit player speed
         if (flatVel.magnitude > maxSpeed)
         {
             Vector3 limitedVel = flatVel.normalized * maxSpeed;
             rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
         }
-
     }
-
-    private void Jump(float jumpMultiplier)
-    {
-        
-        //reset y velocity
-        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
-        
-        rb.AddForce(transform.up * jumpForce * jumpMultiplier, ForceMode.Impulse);
-
-        
-    }
-
-    private void ResetJump() 
-    {
-        readyToJump = true;
-        readyToLongJump = true;
-        longJumping = false;
-           
-    }
-
 }
-
