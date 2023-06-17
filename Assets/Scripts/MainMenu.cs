4dfcf163f512ea5ae9ba22e7adc36baccc74fd71
Tom Keir <tomkeir920@icloud.com>
2023-06-17 11:27:08 +1000

    Pseudocode Algorithms
    Added Pseudocode documents for two key algorithms: MarioMovement and Coins.
    They can be found in the Design Documents folder

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.sceneManagement;
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Play()
     {
-        
+        SceneManager.LoadScene("Game Scene");
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Quit()
     {
-        
+        Application.Quit();
     }
 }
